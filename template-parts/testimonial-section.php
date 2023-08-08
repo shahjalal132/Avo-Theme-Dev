df99b4263276b9b43c1d04c5e21c200860926a9c
Shahjalal <rjshahjalal132@gmail.com>
2023-08-09 00:24:07 +0600

    Testimonial Post type added and loop  done

--- template-parts/testimonial-section.php
+++ template-parts/testimonial-section.php
@@ -11,69 +11,39 @@
         <div class="row justify-content-center wow fadeInUp" data-wow-delay=".5s">
             <div class="col-lg-8">
                 <div class="slic-item">
+
+                    <?php
+                        $testimonial = new WP_Query( array(
+                            'post_type'      => 'testimonial',
+                            'posts_per_page' => -1,
+                        ) );
+
+                        while ( $testimonial->have_posts() ): $testimonial->the_post();
+                    ?>
                     <div class="item">
-                        <p>Working with Avo digital agency to support our organic and paid social media activity has
-                            extended the marketing activities we are able to achieve platform has made collaboration
-                            easy.</p>
+                        <p><?php the_field( 'summery', get_the_ID() );?></p>
                         <div class="info">
                             <div class="cont">
                                 <div class="author">
                                     <div class="lxleft">
                                         <div class="img">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/img/clients/1.jpg"
-                                                alt="">
+                                            <!-- <img src="<?php echo get_template_directory_uri(); ?>/img/clients/1.jpg" alt=""> -->
+                                            <?php the_field( 'image', get_the_ID() );?>
                                         </div>
                                     </div>
                                     <div class="fxright">
-                                        <h6 class="author-name custom-font">Alex Regelman</h6>
-                                        <span class="author-details">Co-founder, Colabrio</span>
+                                        <h6 class="author-name custom-font"><?php the_field( 'name', get_the_ID() );?>
+                                        </h6>
+                                        <span
+                                            class="author-details"><?php the_field( 'profession', get_the_ID() );?></span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="item">
-                        <p>Avo started out managing our SEO efforts, but their scientific approach to digital
-                            marketing and the results they have achieved made it an easy decision for us to handover
-                            the management.</p>
-                        <div class="info">
-                            <div class="cont">
-                                <div class="author">
-                                    <div class="lxleft">
-                                        <div class="img">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/img/clients/2.jpg"
-                                                alt="">
-                                        </div>
-                                    </div>
-                                    <div class="fxright">
-                                        <h6 class="author-name custom-font">Alex Regelman</h6>
-                                        <span class="author-details">Co-founder, Colabrio</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <p>I would highly recommend Avo Digital. I worked with the team on an animation for our
-                            ‘Click & Collect’ service. This team is different from any other agency I have worked
-                            with in the past.</p>
-                        <div class="info">
-                            <div class="cont">
-                                <div class="author">
-                                    <div class="lxleft">
-                                        <div class="img">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/img/clients/3.jpg"
-                                                alt="">
-                                        </div>
-                                    </div>
-                                    <div class="fxright">
-                                        <h6 class="author-name custom-font">Alex Regelman</h6>
-                                        <span class="author-details">Co-founder, Colabrio</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                    <?php endwhile;?>
+
                 </div>
             </div>
         </div>
